4b824bd08c2c15b3e2250f23e84718e750fa60dc
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-04-08 09:10:13 +0530

    added: mobile number logic

--- com/company/Main.java
+++ com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
         Scanner scanner = new Scanner(System.in);
+
         /*System.out.println("Enter the first name(first letter should be capital)");
         String firstName = scanner.next();
         String regex = "^[A-Z][a-z]{3,}$";
@@ -15,11 +16,16 @@ public class Main {
         System.out.println("Enter the last name(first letter should be capital)");
         String lastName = scanner.next();
         String regexLastName = "^[A-Z][a-z]{3,}$";
-        System.out.println(lastName.matches(regexLastName));*/
+        System.out.println(lastName.matches(regexLastName));
 
         System.out.println("Enter a valid email id(eg: abc.xyz@bl.co.in)");
         String emailID = scanner.next();
         String regexEmailID = "^[\\w]+(?:\\.[\\w]+)*@[\\w]+(?:\\.[\\w]+)*$";
-        System.out.println(emailID.matches(regexEmailID));
+        System.out.println(emailID.matches(regexEmailID));*/
+
+        System.out.println("Enter a valid mobile number");
+        String mobileNumber = scanner.next();
+        String regexMobileNumber = "^91[ ][\\d]{10}$";
+        System.out.println(mobileNumber.matches(regexMobileNumber));
     }
 }
