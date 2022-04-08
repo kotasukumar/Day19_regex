4243dc7975b4f0a2d7d91aeb7d4d320a6438c866
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-04-08 07:59:35 +0530

    added:email id logic

--- com/company/Main.java
+++ com/company/Main.java
@@ -6,9 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
-
-        System.out.println("Enter the first name(first letter should be capital)");
         Scanner scanner = new Scanner(System.in);
+        /*System.out.println("Enter the first name(first letter should be capital)");
         String firstName = scanner.next();
         String regex = "^[A-Z][a-z]{3,}$";
         System.out.println(firstName.matches(regex));
@@ -16,6 +15,11 @@ public class Main {
         System.out.println("Enter the last name(first letter should be capital)");
         String lastName = scanner.next();
         String regexLastName = "^[A-Z][a-z]{3,}$";
-        System.out.println(lastName.matches(regexLastName));
+        System.out.println(lastName.matches(regexLastName));*/
+
+        System.out.println("Enter a valid email id(eg: abc.xyz@bl.co.in)");
+        String emailID = scanner.next();
+        String regexEmailID = "^[\\w]+(?:\\.[\\w]+)*@[\\w]+(?:\\.[\\w]+)*$";
+        System.out.println(emailID.matches(regexEmailID));
     }
 }
