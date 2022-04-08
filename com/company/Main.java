e03e43a48c22e0017f400b389f2878d91971d578
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-04-08 07:05:00 +0530

    added:last name logic

--- com/company/Main.java
+++ com/company/Main.java
@@ -12,5 +12,10 @@ public class Main {
         String firstName = scanner.next();
         String regex = "^[A-Z][a-z]{3,}$";
         System.out.println(firstName.matches(regex));
+
+        System.out.println("Enter the last name(first letter should be capital)");
+        String lastName = scanner.next();
+        String regexLastName = "^[A-Z][a-z]{3,}$";
+        System.out.println(lastName.matches(regexLastName));
     }
 }
